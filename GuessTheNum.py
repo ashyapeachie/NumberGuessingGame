214d78d3dbc1d0e7ebfa7d4a6312abfe2521987c
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-07-23 23:41:45 -0400

    while loop(?)

--- GuessTheNum.py
+++ GuessTheNum.py
@@ -7,7 +7,10 @@ def guess_the_number():
     #this function generates a random int between 1 and 100
 
     #printed intro and prompt to guess the right # til correct to the user
-
+    while guess != number_to_guess:
+        try:
+            guess = int(input("Guess a number: "))
+            
     #feedback is given to the user after each guess (too high, too low, getting closer)
 
 #running the progrom(game)
