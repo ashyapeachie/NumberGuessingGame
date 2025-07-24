cad9132ba3925e8c99dac813f2629ee0f91240c6
Ashya <128183143+ashyapeachie@users.noreply.github.com>
2025-07-23 23:39:32 -0400

    starting game

--- GuessTheNum.py
+++ GuessTheNum.py
@@ -1,2 +1,13 @@
 # creation of file for game
-# skilld practiced: random, input, if-else statements, looops
+# skills practiced: random, input, if-else statements, looops
+
+def guess_the_number():
+    number_to_guess = random.randint(1, 100)
+    guess = None
+    #this function generates a random int between 1 and 100
+
+    #printed intro and prompt to guess the right # til correct to the user
+
+    #feedback is given to the user after each guess (too high, too low, getting closer)
+
+#running the progrom(game)
